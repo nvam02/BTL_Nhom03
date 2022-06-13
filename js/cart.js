a12b5b514cc9beac4658e358b9f1c8f438ee7412
vumanhdz <manhtrang2k2@gmail.com>
2022-06-14 00:23:58 +0700

    chinh sua cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,10 @@
 // function show() {
     var img = localStorage.getItem("img")
     var money = localStorage.getItem("money")
-    var name = localStorage.getItem("name")
+    var names = localStorage.getItem("name")
     var anh = JSON.parse(img)
     var tien = JSON.parse(money)
-    var ten = JSON.parse(name)
+    var ten = JSON.parse(names)
     
     var add_div = document.createElement("div")
     var div_content = '<div class="products"><div class="image-products"><img style="height: 160px; width: 120px;" src="'+ anh +'" alt=""></div><div class="infor-products"><a href="">'+ ten +'</a><br><span>M</span><br><span>'+ tien +'₫</span><br><div class="input-group"><button>-</button><input type="text" value="1"><button>+</button></div></div><div class="delete"><a href="">Xóa sản phẩm</a></div></div>'
