10260df9f82919f6457ab2948fa44ae71a731188
vumanhdz <manhtrang2k2@gmail.com>
2022-06-20 20:50:18 +0700

    va loi

--- js/product.js
+++ js/product.js
@@ -7,13 +7,17 @@ const add = document.querySelectorAll("button")
         var productname = product.querySelector("b").innerText
         var productmoney = product.querySelector("span").innerText
         // console.log(productImg, productmoney,productname)
-        let listPD = localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) :[]
+        let listPD = localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) :[]           
+            listPD.map((valua, index)=>{
+               console.log(valua) 
+            })
             listPD.push({
                 img: productImg,
                 names: productname,
                 money: productmoney,
                 SL: 1
             })
+            
         localStorage.setItem("list",JSON.stringify(listPD))
     }})
 })
